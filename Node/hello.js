b30072a410c6004441bc855c4cabdabf35986ea7
JamesMyers0 <james.e.myers@duram.ac.uk>
2019-02-11 13:04:34 +0000

    Added many handlers. Also html form search thing with potatoes.json

---
+++ Node/hello.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+var express = require("express");
+var recipes = require("./potato.json");
+var app = express();
+
+app.get("/", function (req, resp) {
+	resp.send("Hello World!");
+});
+
+app.get("/test", function (req, resp) {
+	resp.send("This is a test. Wowmoo!");
+});
+
+app.get("/wave/:w", function (req, resp) {
+	resp.send("Waving at " + req.params.w);
+});
+
+app.get("/p", function (req, resp) {
+	resp.send("Hello " + req.query.person + "!");
+});
+
+app.get("/s", function (req, resp) {
+	var result = "";
+	for (index=0; index<recipes.length; index++) {
+		if (recipes[index].title.search(req.query.search) != -1) {
+			for (var prop in recipes[index]) {
+				result += recipes[index][prop] + "<br>";
+				result += "<br><br>";
+			}
+		}
+	}
+	resp.send(result);
+});
+
+app.listen(8090);
